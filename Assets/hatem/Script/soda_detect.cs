8496b2056b6cc34b771fc8a742e4b5c62918095c
Hatem <64999740+iHGEN@users.noreply.github.com>
2024-01-01 17:22:05 +0300

    fix error

--- Assets/hatem/Script/soda_detect.cs
+++ Assets/hatem/Script/soda_detect.cs
@@ -7,10 +7,17 @@ public class soda_detect : MonoBehaviour
     [SerializeField] soda_game soda_Game;
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag  == "soda")
+        if(collision.gameObject.tag  == "groung_ball_soda" && this.gameObject.tag == "soda")
         {
-            soda_Game.Soda++;
             gameObject.SetActive(false);
+            soda_Game.Soda++;
+            soda_Game.check_for_win();
+        }
+        if (collision.gameObject.tag == "groung_ball_soda" && this.gameObject.tag == "ball")
+        {
+            gameObject.SetActive(false);
+            soda_Game._ball_count++;
+            soda_Game.check_for_win();
         }
     }
 }
--- Assets/hatem/Script/soda_game.cs
+++ Assets/hatem/Script/soda_game.cs
@@ -10,12 +10,17 @@ public class soda_game : MonoBehaviour
     [SerializeField] GameObject[] _all_object_to_reset;
     [SerializeField] GameObject menu_error;
     [SerializeField] TextMeshProUGUI menu_text;
+    [SerializeField] GameObject _start_button;
     private Vector3[] _ball_location;
     private Vector3[] _soda_location;
+    private Quaternion[] _quaternions;
+    int alert_number = 0;
     void Start()
     {
         _ball_location = new Vector3[_ball.Length];
         _soda_location = new Vector3[_soda.Length];
+        _quaternions = new Quaternion[_soda.Length];
+        get_soda_ball_location();
     }
     void get_soda_ball_location()
     {
@@ -26,6 +31,7 @@ public class soda_game : MonoBehaviour
         for (int x = 0; x < _soda.Length; x++)
         {
             _soda_location[x] = _soda[x].transform.position;
+            _quaternions[x] = _soda[x].transform.rotation;
         }
     }
     void Reset_all()
@@ -36,7 +42,10 @@ public class soda_game : MonoBehaviour
         }
         for (int x = 0; x < _soda.Length; x++)
         {
+            _soda[x].GetComponent<Rigidbody>().isKinematic = true;
             _soda[x].transform.position = _soda_location[x];
+            _soda[x].transform.rotation = _quaternions[x];
+            _soda[x].GetComponent<Rigidbody>().isKinematic = false;
         }
         for(int c = 0; c < _all_object_to_reset.Length;c++)
         {
@@ -49,6 +58,10 @@ public class soda_game : MonoBehaviour
     public void get_start()
     {
         Reset_all();
+        menu_mesg("baling tin Game");
+        _start_button.gameObject.SetActive(true);
+        Soda = 0;
+        _ball_count = 0;
     }
     bool is_run_out_of_ball(GameObject[] ball)
     {
@@ -75,16 +88,46 @@ public class soda_game : MonoBehaviour
     {
         menu_text.text = text;
     }
+   public void opition_index(bool isyes)
+    {
+        Soda = 0;
+        _ball_count = 0;
+        switch (alert_number)
+        {
+            case 0:
+                if (isyes)
+                {
+                    Reset_all();
+                    menu_error.SetActive(false);
+                    return;
+                }
+                menu_error.SetActive(false);
+                break;
+            case 1:
+                if (isyes)
+                {
+                    Reset_all();
+                    menu_error.SetActive(false);
+                    return;
+                }
+                get_start();
+                menu_error.SetActive(false);
+                break;
+        }
+    }
     public void check_for_win()
     {
-        if (Soda != 6 && is_run_out_of_ball(_ball))
+        if (Soda != 6 & is_run_out_of_ball(_ball))
         {
+            _start_button.gameObject.SetActive(false);
             menu_mesg("You Lost \r\n\r\n do you want to try again ?");
+            alert_number = 0;
             menu_error.SetActive(true);
             return;
         }
         if(Soda == 6)
         {
+            alert_number = 1;
             menu_mesg("You Win");
             menu_error.SetActive(true);
         }
