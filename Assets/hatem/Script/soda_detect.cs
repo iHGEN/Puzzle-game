236eaf00ccdccbb293aaa2e04f34ae6df205aaf5
Hatem <64999740+iHGEN@users.noreply.github.com>
2024-01-01 17:27:34 +0300

    Revert "fix error"
    This reverts commit 8496b2056b6cc34b771fc8a742e4b5c62918095c.

--- Assets/hatem/Script/soda_detect.cs
+++ Assets/hatem/Script/soda_detect.cs
@@ -7,17 +7,10 @@ public class soda_detect : MonoBehaviour
     [SerializeField] soda_game soda_Game;
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag  == "groung_ball_soda" && this.gameObject.tag == "soda")
+        if(collision.gameObject.tag  == "soda")
         {
-            gameObject.SetActive(false);
             soda_Game.Soda++;
-            soda_Game.check_for_win();
-        }
-        if (collision.gameObject.tag == "groung_ball_soda" && this.gameObject.tag == "ball")
-        {
             gameObject.SetActive(false);
-            soda_Game._ball_count++;
-            soda_Game.check_for_win();
         }
     }
 }
