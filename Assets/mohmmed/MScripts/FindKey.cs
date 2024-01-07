d66bf36d3b2a4cfa1b2e400b846fa6a71fe2533a
Hatem <64999740+iHGEN@users.noreply.github.com>
2024-01-07 03:41:16 +0300

    fix error

--- Assets/mohmmed/MScripts/FindKey.cs
+++ Assets/mohmmed/MScripts/FindKey.cs
@@ -3,41 +3,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyGrabInteraction : MonoBehaviour
 {
-    public ParticleSystem grabParticleSystem; 
 
     private XRGrabInteractable grabInteractable;
+    [SerializeField] coins _coins;
 
     void Start()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
-
-        
-        if (grabParticleSystem == null)
-        {
-            
-            return;
-        }
-
         grabInteractable.selectEntered.AddListener(HandleGrab);
-        grabInteractable.selectExited.AddListener(HandleRelease);
     }
 
     private void HandleGrab(SelectEnterEventArgs args)
     {
-        grabParticleSystem.Play(); 
+        _coins.add_coins(2);
+        Destroy(gameObject);
     }
 
-    private void HandleRelease(SelectExitEventArgs args)
-    {
-        grabParticleSystem.Stop(); 
-    }
 
     void OnDestroy()
     {
         if (grabInteractable != null)
         {
             grabInteractable.selectEntered.RemoveListener(HandleGrab);
-            grabInteractable.selectExited.RemoveListener(HandleRelease);
         }
     }
 }
